5a224b103add2a1a15df8f9fad4a79ebdef872f3
mzlicic <milan.zlicic@wolkabout.com>
2019-04-01 14:30:43 +0200

    Send file list on change

--- src/service/FileDownloadService.cpp
+++ src/service/FileDownloadService.cpp
@@ -524,6 +524,8 @@ void FileDownloadService::downloadCompleted(const std::string& fileName, const s
         m_fileRepository.store(FileInfo{fileName, fileHash, filePath});
         sendStatus(FileUploadStatus{fileName, FileTransferStatus::FILE_READY});
     });
+
+    sendFileList();
 }
 
 void FileDownloadService::downloadFailed(const std::string& fileName, FileTransferError errorCode)
@@ -531,6 +533,8 @@ void FileDownloadService::downloadFailed(const std::string& fileName, FileTransf
     flagCompletedDownload(fileName);
 
     sendStatus(FileUploadStatus{fileName, errorCode});
+
+    sendFileList();
 }
 
 void FileDownloadService::urlDownloadCompleted(const std::string& fileUrl, const std::string& fileName,
@@ -552,11 +556,15 @@ void FileDownloadService::urlDownloadCompleted(const std::string& fileUrl, const
         m_fileRepository.store(FileInfo{fileName, hashStr, filePath});
         sendStatus(FileUrlDownloadStatus{fileUrl, fileName});
     });
+
+    sendFileList();
 }
 
 void FileDownloadService::urlDownloadFailed(const std::string& fileUrl, FileTransferError errorCode)
 {
     sendStatus(FileUrlDownloadStatus{fileUrl, errorCode});
+
+    sendFileList();
 }
 
 void FileDownloadService::addToCommandBuffer(std::function<void()> command)
